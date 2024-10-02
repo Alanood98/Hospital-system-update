f087e137e6e906e2c9b7021784a610f642ebc760
Alanoud <Alanoud2022it@gmail.com>
2024-10-02 16:31:56 +0400

    add methods

--- Patient.cs
+++ Patient.cs
@@ -1,24 +1,22 @@
-﻿
-
-namespace HospitalClassINhernite
+﻿namespace HospitalClassINhernite
 {
-    public class Patient:Person
+    public class Patient : Person
     {
-        
+
         public int PatientID;
         public string Ailment;
         public Doctor AssignedDoctor { get; private set; }
-        public Room ?AssignedRoom { get; set; }
+        public Room? AssignedRoom { get; set; }
 
-        public Patient(int pationid,string name, int age, Gender gender,string ailmenrt,Doctor doctor):base(name, age, gender)
+        public Patient(int pationid, string name, int age, Gender gender, string ailmenrt, Doctor doctor) : base(name, age, gender)
         {
             PatientID = pationid;
             Ailment = ailmenrt;
             AssignedDoctor = doctor;
 
-        
-           
-            
+
+
+
 
         }
         public override void DisplayInfo()
