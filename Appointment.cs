f1884c4b4e64b3f38d61f72697b5757c85862ff3
Alanoud <Alanoud2022it@gmail.com>
2024-10-02 16:33:28 +0400

    add methods

--- Appointment.cs
+++ Appointment.cs
@@ -1,37 +1,49 @@
-﻿
-using System.Numerics;
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace HospitalClassINhernite
 {
     public class Appointment
     {
 
-        public Patient patient;
+        public Patient patient=null;
         public Doctor doctor;
-        public DateTime ?AppointmentDate;
+        public DateTime? AppointmentDate;
         public TimeSpan AppointmentTime;
         public bool IsBooked;
-        public Appointment(Patient patient, Doctor doctor,DateTime dateappo, TimeSpan appointmentTime , bool isBooked)
+        public Appointment(Patient patient, Doctor doctor, DateTime dateappo, TimeSpan appointmentTime, bool isBooked)
         {
             this.patient = patient;
             this.doctor = doctor;
-            AppointmentDate=dateappo;
+            AppointmentDate = dateappo;
             AppointmentTime = appointmentTime;
             IsBooked = isBooked;
         }
-
-        public void ScheduleAppointment(DateTime? dateappo, TimeSpan appointmentTime)
+        public Appointment()
         {
+            
+        }
+
+        public void ScheduleAppointment(Patient patient, DateTime? dateappo, TimeSpan appointmentTime,bool IsBooked)
+        {
+            this.patient = patient;
             AppointmentDate = dateappo;
             AppointmentTime = appointmentTime;
             IsBooked = true;  // Mark as booked when scheduled
         }
+        public void ScheduleAppointment( DateTime? dateappo, TimeSpan appointmentTime)
+        {
+            
+            AppointmentDate = dateappo;
+            AppointmentTime = appointmentTime;
+       
+        }
 
-
-        public void CancelAppointment()
+        public void CancelAppointment(DateTime date, TimeSpan period)
         {
             AppointmentDate = null;
             AppointmentTime = TimeSpan.Zero;  // Clear the appointment time
+            this.patient = null;
             IsBooked = false; // Mark as not booked
 
         }
@@ -41,7 +53,7 @@ namespace HospitalClassINhernite
         {
             Console.WriteLine($"Appointment scheduled for {patient.Name} on {AppointmentDate.Value:MMMM dd, yyyy} at {AppointmentTime:hh\\:mm}");
         }
-       
+
 
 
 
