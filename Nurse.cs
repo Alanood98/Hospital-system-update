774fa439b5c442361ef17dcb533caf88ffc06060
Alanoud <Alanoud2022it@gmail.com>
2024-10-07 15:55:51 +0400

    add these methods : AssistDoctor,CheckVitals,AdministerMedication

--- Nurse.cs
+++ Nurse.cs
@@ -10,7 +10,7 @@ namespace HospitalClassINhernite
     {
         public int NurseID;
         public Clinic AssignedClinic;
-        public Specialization nurseSpec;
+        public Specialization NurseSpec;
         public enum Specialization
         {
             General, 
@@ -18,5 +18,28 @@ namespace HospitalClassINhernite
             Surgery
         }
         public static List<Patient> NursePatients = new List<Patient>();
+
+        public Nurse(int nurseID, Clinic assignedClinic, Specialization nurseSpec,string name,int age,Gender gender) : base(name, age, gender)
+        {
+            NurseID = nurseID;
+            AssignedClinic = assignedClinic;
+            NurseSpec = nurseSpec;
+
+        }
+        public void AssistDoctor(Doctor doctor, Patient patient)
+        {
+
+        }
+        public void CheckVitals(Patient patient)
+        {
+
+        }
+
+        public void AdministerMedication(Patient patient, string medication)
+        {
+
+        }
+
+        
     }
 }
