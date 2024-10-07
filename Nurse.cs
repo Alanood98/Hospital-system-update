f4617f5b50aae397d1f01e78c3c87b2cbefe296d
Alanoud <Alanoud2022it@gmail.com>
2024-10-07 15:44:37 +0400

    add Attributes in nurse class

--- Nurse.cs
+++ Nurse.cs
@@ -8,6 +8,15 @@ namespace HospitalClassINhernite
 {
     public class Nurse : Person
     {
-
+        public int NurseID;
+        public Clinic AssignedClinic;
+        public Specialization nurseSpec;
+        public enum Specialization
+        {
+            General, 
+            Pediatrics, 
+            Surgery
+        }
+        public static List<Patient> NursePatients = new List<Patient>();
     }
 }
